56bfcf0c7524c682c886143d36478e344edfdafa
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2024-11-20 17:29:19 -0300

    Saving passwords to a .txt file

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from tkinter import *
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def clear():
+    website_entry.delete(0, 'end')
+    email_entry.delete(0, 'end')
+    password_entry.delete(0, 'end')
+
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    try:
+        with open("data.txt", "a") as datafile:
+            datafile.write(f"{website} | {email} | {password}\n")
+        clear()
+    except Exception as e:
+        print(f"An exception occurred {e}")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -23,27 +42,26 @@ username_label.grid(column=0, row=2, columnspan=1)
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3, columnspan=1)
 
-
 # Entries
-website_input = Entry()
-website_input.config(width=41)
-website_input.grid(row=1, column=1, columnspan=2, sticky="w")
+website_entry = Entry()
+website_entry.config(width=41)
+website_entry.grid(row=1, column=1, columnspan=2, sticky="w")
+website_entry.focus()
 
-username_input = Entry()
-username_input.config(width=41)
-username_input.grid(row=2, column=1, columnspan=1, sticky="w")
+email_entry = Entry()
+email_entry.config(width=41)
+email_entry.grid(row=2, column=1, columnspan=1, sticky="w")
+# email_entry.insert(0, "krm.sp.08@gmail.com")
 
-password_input = Entry(width=23)
-password_input.grid(row=3, column=1, columnspan=1, sticky="w")
-
+password_entry = Entry(width=23)
+password_entry.grid(row=3, column=1, columnspan=1, sticky="w")
 
 #Button
 password_button = Button(text="Generate Password")
-password_button.grid(row=3, column=1,sticky="e")
+password_button.grid(row=3, column=1, sticky="e")
 
 add_button = Button(text="Add")
-add_button.config(width=35)
+add_button.config(width=35, command=save)
 add_button.grid(column=1, row=4, columnspan=2, sticky="w")
-
 
 window.mainloop()
